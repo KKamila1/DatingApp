05015c292457c412e69a70d25796bef1911a4197
KKamila1 <kotrys.kamila@gmail.com>
2020-03-03 18:12:23 +0100

    first

---
+++ DatingAPI/DatingAPI/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+        public ValuesController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        //Get api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values =await dataContext.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        //reszta
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var values = await dataContext.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(values);
+        }
+    }
+}
